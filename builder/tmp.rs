b019c9e6c1c9258c63b68eb48f214cdd4c33f9f4
Alexandre Bonavita <alexandre.bonavita@tehtris.com>
2025-03-17 18:32:05 +0100

    test 2 + progress

--- builder/tmp.rs
+++ builder/tmp.rs
@@ -3,103 +3,32 @@
 use std::prelude::rust_2021::*;
 #[macro_use]
 extern crate std;
-use derive_builder::Builder;
-pub struct Command {
-    executable: String,
-    #[builder(each = "arg")]
-    args: Vec<String>,
-    #[builder(each = "env")]
-    env: Vec<String>,
-    current_dir: Option<String>,
+use derive_debug::CustomDebug;
+pub struct Field {
+    name: &'static str,
+    #[debug = "0b{:08b}"]
+    bitmask: u8,
 }
-use std::error::Error;
-fn test() {
-    let a = "String";
-    let a = "Vec";
-    let a = "Vec";
-    let a = "Option";
-}
-pub struct CommandBuilder {
-    executable: Option<String>,
-    args: Vec<String>,
-    env: Vec<String>,
-    current_dir: Option<String>,
-}
-impl CommandBuilder {
-    fn executable(&mut self, value: String) -> &mut Self {
-        self.executable = std::option::Option::Some(value);
-        self
-    }
-    fn args(&mut self, value: Vec<String>) -> &mut Self {
-        self.args = value;
-        self
-    }
-    fn current_dir(&mut self, value: String) -> &mut Self {
-        self.current_dir = std::option::Option::Some(value);
-        self
-    }
-    fn arg(&mut self, value: String) -> &mut Self {
-        self.args.push(value);
-        self
-    }
-    fn env(&mut self, value: String) -> &mut Self {
-        self.env.push(value);
-        self
-    }
-    pub fn build(&mut self) -> std::result::Result<Command, std::boxed::Box<dyn Error>> {
-        if let CommandBuilder {
-            executable: std::option::Option::Some(executable),
-            args,
-            env,
-            current_dir,
-        } = self {
-            std::result::Result::Ok(Command {
-                executable: executable.to_owned(),
-                args: args.to_owned(),
-                env: env.to_owned(),
-                current_dir: current_dir.to_owned(),
-            })
-        } else {
-            std::result::Result::Err(
-                std::boxed::Box::from("Could not build because of missing attributes."),
-            )
-        }
-    }
-}
-impl Command {
-    pub fn builder() -> CommandBuilder {
-        CommandBuilder {
-            executable: std::option::Option::None,
-            args: std::vec::Vec::new(),
-            env: std::vec::Vec::new(),
-            current_dir: std::option::Option::None,
-        }
+use std::fmt;
+impl fmt::Debug for Field {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Field")
+            .field("name", &self.name)
+            .field("bitmask", &self.bitmask)
+            .finish()
     }
 }
 fn main() {
-    let command = Command::builder()
-        .executable("cargo".to_owned())
-        .arg("build".to_owned())
-        .arg("--release".to_owned())
-        .build()
-        .unwrap();
-    match (&command.executable, &"cargo") {
-        (left_val, right_val) => {
-            if !(*left_val == *right_val) {
-                let kind = ::core::panicking::AssertKind::Eq;
-                ::core::panicking::assert_failed(
-                    kind,
-                    &*left_val,
-                    &*right_val,
-                    ::core::option::Option::None,
-                );
-            }
-        }
+    let f = Field {
+        name: "F",
+        bitmask: 0b00011100,
     };
-    match (
-        &command.args,
-        &<[_]>::into_vec(::alloc::boxed::box_new(["build", "--release"])),
-    ) {
+    let debug = ::alloc::__export::must_use({
+        let res = ::alloc::fmt::format(format_args!("{0:?}", f));
+        res
+    });
+    let expected = r#"Field { name: "F", bitmask: 0b00011100 }"#;
+    match (&debug, &expected) {
         (left_val, right_val) => {
             if !(*left_val == *right_val) {
                 let kind = ::core::panicking::AssertKind::Eq;
--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,128 @@
 use proc_macro::TokenStream;
+use syn::{self, spanned::Spanned};
+use quote::{quote, quote_spanned};
 
-#[proc_macro_derive(CustomDebug)]
+
+struct CompileError<'a> {
+    message: &'a str,
+    span: proc_macro2::Span,
+}
+
+impl<'a> CompileError<'a> {
+    fn to_token(&self) -> proc_macro2::TokenStream {
+        let message = self.message;
+        quote_spanned! { self.span => compile_error!(#message) }
+    }
+}
+
+
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let name = input.ident;
+    let name_str = syn::LitStr::new(&name.to_string(), name.span());
 
-    unimplemented!()
+    let fields = if let syn::Data::Struct(data) = input.data {
+        data.fields
+    } else { 
+        return quote! { compile_error!("Expected struct input") }.into()
+    };
+
+    let named_fields: Vec<syn::Field> = if let syn::Fields::Named(f) = fields {
+        f.named.into_iter().collect()
+    } else {
+        return quote! { compile_error!("Expected named fields") }.into()
+    };
+
+    let debug_fields = named_fields.iter().map(create_debug_field_impl);
+
+    quote! {
+        use std::fmt;
+        
+        impl fmt::Debug for #name {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.debug_struct(#name_str)
+                 #(#debug_fields)*
+                 .finish()
+            }
+        }
+    }.into()
+}
+
+
+fn extract_format_string(attr: &syn::Attribute) -> Result<syn::LitStr, CompileError> {
+    let expr: syn::Expr = attr.parse_args()
+        .or(
+            Err(
+                CompileError {
+                    message: "Can't parse the attribute's argument",
+                    span: attr.span(),
+        }))?;
+
+    let expr_assign = if let syn::Expr::Assign(e) = expr {
+        e
+    } else {
+        return Err(
+            CompileError {
+                message: "The expression is not an assignment",
+                span: expr.span(),
+    })};
+
+    if let syn::Expr::Path(expr_path) = *expr_assign.left {
+        if !expr_path.path.is_ident("debug") {
+            return Err(
+                CompileError {
+                    message: "The left part of the expression is not 'debug'",
+                    span: expr_path.path.span(),
+        })};
+    } else {
+        return Err(
+            CompileError{
+                message: "The left part of the expression is not a path",
+                span: expr_assign.left.span()
+    })};
+
+    if let syn::Expr::Lit(lit_expr) = *expr_assign.right {
+        if let syn::Lit::Str(lit_str) = lit_expr.lit {
+            Ok(lit_str)
+        } else {
+            return Err(CompileError {
+                message: "The right part of the expression is not a string",
+                span: lit_expr.lit.span(),
+        })}
+    } else {
+        return Err(
+            CompileError {
+                message: "The right part of the expression is not a literal",
+                span: expr_assign.right.span(),
+    })}
+}
+
+
+fn create_debug_field_impl(field: &syn::Field) -> proc_macro2::TokenStream {
+    let ident_result = field.ident
+        .to_owned()
+        .ok_or(
+            CompileError { 
+                span: field.ident.span(),
+                message: "Field has no identifier"});
+    
+    let ident = match ident_result {
+        Ok(value) => value,
+        Err(err) => return err.to_token(),
+    };
+
+    let ident_str = syn::LitStr::new(&ident.to_string(), field.span());
+
+    let format_str_option = field.attrs.iter().filter_map(|attr| extract_format_string(attr).ok()).last();
+
+    if let Some(format_str) = format_str_option {
+        quote! {
+            .field(#ident_str, format!(#format_str, &self.#ident))
+        }
+    } else {
+        quote! {
+            .field(#ident_str, &self.#ident)
+        }
+    }
 }
